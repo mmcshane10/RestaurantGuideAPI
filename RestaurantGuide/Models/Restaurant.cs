337de8c3813929c1750fb26f7dc59697daab2a50
Student <example@example.com>
2019-10-29 11:43:20 -0700

    add validations to restaurant class

--- RestaurantGuide/Models/Restaurant.cs
+++ RestaurantGuide/Models/Restaurant.cs
@@ -1,12 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace RestaurantGuide.Models
 {
     public class Restaurant
     {
         public int RestaurantId { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Cuisine { get; set; }
+        [Required]
         public string Address { get; set; }
+        [Required]
         public string Neighborhood { get; set; }
+        [Required]
         public string Quadrant { get; set; }
     }
 }
